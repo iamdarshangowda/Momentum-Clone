05941aaa868ecf392564d4c6cfc8e3c36335d358
iamdarshangowda <iamdarshangowda@gmail.com>
2022-07-20 00:06:56 +0530

    time added

--- src/components/DigitalClock.js
+++ src/components/DigitalClock.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function DigitalClock() {
-  return <div className="clock">DigitalClock</div>;
+  const [time, setTime] = useState(new Date().toLocaleTimeString().slice(0, 5));
+
+  useEffect(() => {
+    const timeId = setInterval(
+      () => setTime(new Date().toLocaleTimeString().slice(0, 5)),
+      1000
+    );
+    return function () {
+      clearInterval(timeId);
+    };
+  });
+
+  return (
+    <div className="time-container">
+      <h1 className="time">{time}</h1>
+    </div>
+  );
 }
